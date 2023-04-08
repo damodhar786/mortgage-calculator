deb7600b41f497bcf716449d0c8e5348bc0a2bbc
damodhar <76522052+damodhar786@users.noreply.github.com>
2023-04-08 19:48:19 +0530

    slider select loan value

--- src/components/SliderSelect.js
+++ src/components/SliderSelect.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React from 'react';
+import SliderComponent from './common/SliderComponent';
 
-function SliderSelect() {
+const SliderSelect = ({ data, setData }) => {
+  console.log(data, 'Data');
+
+  const bank_limit = 10000;
   return (
     <div>
-      <h1>Slider Select</h1>
+      <SliderComponent
+        onChange={(e, value) => {
+          setData({
+            ...data,
+            homeValue: value.toFixed(0),
+            downPayment: (0.2 * value).toFixed(0),
+            loanAmount: (0.8 * value).toFixed(0),
+          });
+          console.log(data, 'Set Data');
+        }}
+        defaultValue={data.homeValue}
+        min={1000}
+        max={bank_limit}
+        steps={100}
+        unit="$"
+        amount={data.homeValue}
+        label="Home Value"
+        value={data.homeValue}
+      />
     </div>
-  )
-}
+  );
+};
 
 export default SliderSelect
