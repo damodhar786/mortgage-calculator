81b8d9bef7dfc4cadf27a349cb6fcc9af486a049
damodhar <76522052+damodhar786@users.noreply.github.com>
2023-04-09 08:09:59 +0530

    downPayment, loanAmount and interestRate sliders

--- src/components/SliderSelect.js
+++ src/components/SliderSelect.js
@@ -7,6 +7,7 @@ const SliderSelect = ({ data, setData }) => {
   const bank_limit = 10000;
   return (
     <div>
+      {/* Home Value */}
       <SliderComponent
         onChange={(e, value) => {
           setData({
@@ -26,6 +27,62 @@ const SliderSelect = ({ data, setData }) => {
         label="Home Value"
         value={data.homeValue}
       />
+
+      {/* Down Payment */}
+
+      <SliderComponent
+        onChange={(e, value) =>
+          setData({
+            ...data,
+            downPayment: value.toFixed(0),
+            loanAmount: (data.homeValue - value).toFixed(0),
+          })
+        }
+        defaultValue={data.downPayment}
+        min={0}
+        max={data.homeValue}
+        steps={100}
+        unit="$"
+        amount={data.downPayment}
+        label="Down Payment"
+        value={data.downPayment}
+      />
+
+      <SliderComponent
+        onChange={(e, value) =>
+          setData({
+            ...data,
+            loanAmount: value.toFixed(0),
+            downPayment: (data.homeValue - value).toFixed(0),
+          })
+        }
+        defaultValue={data.loanAmount}
+        min={0}
+        max={data.homeValue}
+        steps={100}
+        unit="$"
+        amount={data.loanAmount}
+        label="Loan Amount"
+        value={data.loanAmount}
+      />
+
+      <SliderComponent 
+        onChange={(e,value) =>
+          setData({
+            ...data,
+            interestRate: value,
+          })
+        }
+        defaultValue={data.interestRate}
+        min={2}
+        max={18}
+        steps={0.5}
+        unit="$"
+        amount={data.interestRate}
+        label="Interest Rate"
+        value={data.interestRate}
+      />
+
     </div>
   );
 };
